5b9a880f12a214b0b598fc078b6fc439e28fdbe0
wuyang-liu <liuwuyayui@gmail.com>
2022-02-23 23:03:41 -0800

    feat: init commit

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+    SafeAreaView,
+    StatusBar,
+} from "react-native";
+
+const App: () => Node = () => {
+    return (
+        <>
+            <StatusBar barStyle="dark-content"/>
+            <SafeAreaView>
+            </SafeAreaView>
+        </>
+    );
+};
+
+export default App;
